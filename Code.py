0301428fe545c61711d5aac84b1afe507a7fc0f4
Specialized123 <53801574+Specialized123@users.noreply.github.com>
2021-02-24 16:25:52 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,8 +1,6 @@
-
-##########调用摄像头版本###########
-
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 ball_color = 'green'                       #目标颜色
 
@@ -24,12 +22,18 @@ while cap.isOpened():
             hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)                 # 转化成HSV图像
             erode_hsv = cv2.erode(hsv, None, iterations=2)                   # 降噪
             inRange_hsv = cv2.inRange(erode_hsv, color_dist[ball_color]['Lower'], color_dist[ball_color]['Upper']) #去除背景，保留目标即可
-            cnts = cv2.findContours(inRange_hsv.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+            contours , hierarchy = cv2.findContours(inRange_hsv.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
 
-            c = max(cnts, key=cv2.contourArea)
-            rect = cv2.minAreaRect(c)
-            box = cv2.boxPoints(rect)
-            cv2.drawContours(frame, [np.int0(box)], -1, (0, 255, 255), 2)
+            
+            # plt.subplot(1,2,1); plt.imshow(erode_hsv);plt.axis('off');plt.title('eHSV')
+            # plt.subplot(1,2,1); plt.imshow(hsv);plt.axis('off');plt.title('HSV')
+            # plt.subplot(1,2,2); plt.imshow(inRange_hsv);plt.axis('off');plt.title('mask')
+            # plt.show()
+            #图像对比测试
+
+
+    
+            cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
             #绘制目标矩形轮廓
 
             cv2.imshow('camera', frame)
@@ -42,37 +46,4 @@ while cap.isOpened():
 cap.release()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-
-###########识别图片版本############
-import cv2
-import numpy as np
-
-ball_color = 'green'
-
-color_dist = {'red': {'Lower': np.array([0, 60, 60]), 'Upper': np.array([6, 255, 255])},
-              'blue': {'Lower': np.array([100, 80, 46]), 'Upper': np.array([124, 255, 255])},
-              'green': {'Lower': np.array([35, 43, 35]), 'Upper': np.array([90, 255, 255])},
-              }
-
-frame = cv2.imread('D:\\Robomaster\\datasets_to\\gyz\\origin\\IMG_20210122_161729.jpg')
-
-gs_frame = cv2.GaussianBlur(frame, (5, 5), 0)                     # 高斯模糊
-hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)                 # 转化成HSV图像
-erode_hsv = cv2.erode(hsv, None, iterations=2)                   #降噪
-inRange_hsv = cv2.inRange(erode_hsv, color_dist[ball_color]['Lower'], color_dist[ball_color]['Upper'])
-cnts = cv2.findContours(inRange_hsv.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-
-c = max(cnts, key=cv2.contourArea)
-rect = cv2.minAreaRect(c)
-box = cv2.boxPoints(rect)
-cv2.drawContours(frame, [np.int0(box)], -1, (0, 255, 255), 2)
-
-cv2.imshow('camera', frame)
-
-cv2.waitKey(0)
 
